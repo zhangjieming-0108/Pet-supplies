d2b06d73047cccd6fef797907665d7e142fdf8d2
zhangjieming0108 <18816799432@163.com>
2019-08-25 19:57:55 +0800

    增加了底部导航栏和选中的面板

---
+++ src/components/Index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <!-- 选中要显示的面板 -->
+        <mt-tab-container v-model="selected">
+            <mt-tab-container-item id="index">
+                    首页
+            </mt-tab-container-item>
+            <mt-tab-container-item id="kind">
+                    分类
+            </mt-tab-container-item>
+            <mt-tab-container-item id="shop">
+                    购物
+            </mt-tab-container-item>
+            <mt-tab-container-item id="me">
+                    我的
+            </mt-tab-container-item>
+        </mt-tab-container>
+        <!-- 底部导航栏 -->
+        <mt-tabbar v-model="selected" fixed>
+            <mt-tab-item id="index">
+                <img slot="icon" src="@/assets/home.png">
+                首页
+            </mt-tab-item>
+            <mt-tab-item id="kind">
+                <img slot="icon" src="@/assets/kind.png">
+                分类
+            </mt-tab-item>
+            <mt-tab-item id="shop">
+                <img slot="icon" src="@/assets/shop.png">
+                购物
+            </mt-tab-item>
+            <mt-tab-item id="me">
+                <img slot="icon" src="@/assets/user.png">
+                我的
+            </mt-tab-item>
+</mt-tabbar>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                selected:"index"
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 完整引入mint-ui组件库中的所有组件
+import MintUI from 'mint-ui'
+// 2.单独引入mint-ui组件中的样式文件
+import "mint-ui/lib/style.css"
+// 注册mint-ui
+Vue.use(MintUI)
+// 配置axios，引入第三方ajax请求模块
+import axios from 'axios'
+// 注册axios，由于axios不支持use将实列添加vue原型
+Vue.prototype.axios=axios;
+// 配置axios的基础路径
+//axios.defaults,baseURL="http://127.0.0.1:3000/"
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
